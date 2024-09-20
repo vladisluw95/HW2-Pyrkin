57259c9acfa979e9fa8fc2c67ff336c0b60ce8b1
privetttppoka <nazarova.al000@gmail.com>
2024-09-20 21:12:24 +0300

    Add substract and main

--- HW2-Pyrkin/calculator.py
+++ HW2-Pyrkin/calculator.py
@@ -1,3 +1,7 @@
+def subst(a,b):
+    #Вычитание
+    return a - b 
+
 def mult(a,b):
     #Умножение
     return a * b
@@ -9,3 +13,26 @@ def plus(a,b):
     #Сложение
     return a + b
 
+
+def main():
+    expression = input('Введите математическое выражение, используя 2 переменные и символы "+", "-", "*", "/"')
+    some_parts = expression.split()
+    a, operator, b = some_parts
+    a = float(a)
+    b = float(b)
+    if (operator == '*'):
+        result = mult(a, b)
+    elif (operator == '/'):
+        result = div(a, b)
+    elif (operator == '-'):
+        result = subst(a, b)
+    elif (operator == '+'):
+        result = plus(a, b)
+
+    
+
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
